5ccd5e185e28bc55b2d2d04a52da01e974fd2fea
Gungrave <jorgefranciscocz@gmail.com>
2024-10-09 00:24:11 -0600

    New insert

--- app/src/main/java/sv/edu/catolica/pianogrupo01/MainActivity.java
+++ app/src/main/java/sv/edu/catolica/pianogrupo01/MainActivity.java
@@ -1,14 +1,21 @@
 package sv.edu.catolica.pianogrupo01;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button about, exit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,26 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getTitle().toString()){
+            case "Acerca de nosotros":
+                Intent intent = new Intent(this, About_us.class);
+                startActivity(intent);
+                return true;
+            case "Salir":
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
